b2d4fcdb69bd49488af9776229367b996cfe450f
tetunori <tetunori@gmail.com>
2023-04-15 21:28:36 +0900

    completed AR graphics sample

--- sample/createARGraphics/sketch.js
+++ sample/createARGraphics/sketch.js
@@ -1,17 +1,29 @@
+const gfxs = [];
+const W = 240;
 
-let gfx1, gfx2;
+let t = 0;
+const cols = ['#01295f', '#437f97', '#849324', '#ffb30f', '#fd151b', 120];
 
 function setup() {
-  createCanvas(720, 720);
-  gfx1 = createARGraphics(720, 720, P2D, params = {scale:3, opacity:1.0, markerId:1 });
-  gfx2 = createARGraphics(720, 720, P2D, params = {scale:3, opacity:1.0, markerId:6 });
+  for (let i = 0; i < 6; i++) {
+    const gfx = createARGraphics(W, W, P2D, { scale: 1, markerId: i });
+    gfx.noStroke();
+    gfxs.push(gfx);
+  }
 }
 
 function draw() {
-  gfx1.background(220);
-  gfx1.fill(0);
-  gfx1.circle(cos(frameCount * 0.02) * 250 + width / 2, sin(frameCount * 0.02) * 250 + height / 2, 100);
-  gfx2.background('red');
-  gfx2.fill('blue');
-  gfx2.circle(cos(frameCount * 0.02) * 250 + width / 2, sin(frameCount * 0.02) * 250 + height / 2, 100);
+  gfxs.forEach((gfx, index) => {
+    gfx.clear();
+    gfx.fill(cols[index]);
+    gfx.circle(W / 2, W / 2, W);
+    gfx.fill(255);
+    const n = index + 1;
+    for (let i = 0; i < n; i++) {
+      const a = (i * PI) / n;
+      v = createVector(cos(a), sin(a)).mult((W / 3) * sin(t + a));
+      gfx.circle(v.x + W / 2, v.y + W / 2, W / 15);
+    }
+  });
+  t += 0.04;
 }
