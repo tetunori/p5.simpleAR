dbd9ee58b58269956c59da4c403a15d355577320
tetunori <tetunori@gmail.com>
2023-04-15 10:30:00 +0900

    Brush up the code

--- dist/v0.6.0/p5SimpleAR.js
+++ dist/v0.6.0/p5SimpleAR.js
@@ -1,3 +1,4 @@
+// Load scripts aframe and ar.js
 const aFrameScript = document.createElement('script');
 aFrameScript.src = 'https://aframe.io/releases/1.4.0/aframe.min.js';
 aFrameScript.onload = () => {
@@ -15,82 +16,22 @@ aFrameScript.onload = () => {
 };
 document.head.appendChild(aFrameScript);
 
-const createARCanvas = (w, h, renderer = P2D, params = { scale: 3, opacity: 1.0, markerId: 6 }) => {
-  // console.log('createARCanvas');
+// API: createARCanvas
+const createARCanvas = (w, h, renderer = P2D, params) => {
   const cvs = createCanvas(w, h, renderer);
-
-  if (!params.hasOwnProperty('scale')) {
-    params.scale = 3;
-  }
-
-  if (!params.hasOwnProperty('opacity')) {
-    params.opacity = 1.0;
-  }
-  params.opacity = constrain(params.opacity, 0.0, 1.0);
-
-  if (!params.hasOwnProperty('markerId')) {
-    params.markerId = 6;
-  }
-  params.markerId = constrain(params.markerId, 0, 63);
-
-  cvs.id('p5Canvas' + String(params.markerId));
-  cvs.parent('a-assets');
-
-  if (document.querySelector('#p5SimpleAR-plane' + String(params.markerId)) === null) {
-    let arW, arH;
-    const scaleUnit = params.scale;
-    if (arW < arH) {
-      arW = (scaleUnit * w) / h;
-      arH = scaleUnit;
-    } else {
-      arW = scaleUnit;
-      arH = (scaleUnit * h) / w;
-    }
-    const atts = [
-      ['position', '0 0 0'],
-      ['rotation', '-90 0 0'],
-      ['width', String(arW)],
-      ['height', String(arH)],
-      [
-        'material',
-        'src: #p5Canvas' +
-          String(params.markerId) +
-          '; transparent: true; opacity: ' +
-          String(params.opacity) +
-          ';',
-      ],
-    ];
-    const plane = document.createElement('a-plane');
-    plane.id = 'p5SimpleAR-plane' + String(params.markerId);
-    atts.forEach((att) => plane.setAttribute(att[0], att[1]));
-
-    const scene = document.querySelector('a-scene');
-    const marker = document.createElement('a-marker');
-    const markerAtts = [
-      ['id', 'a-marker' + String(params.markerId)],
-      ['type', 'barcode'],
-      ['value', String(params.markerId)],
-      ['registerevents', ''],
-    ];
-    markerAtts.forEach((markerAtt) => marker.setAttribute(markerAtt[0], markerAtt[1]));
-    marker.appendChild(plane);
-    scene.appendChild(marker);
-
-    replaceARDraw();
-  }
-
+  p5SimpleARCreateARCore(w, h, cvs, params);
   return cvs;
 };
 
-const createARGraphics = (
-  w,
-  h,
-  renderer = P2D,
-  params = { scale: 3, opacity: 1.0, markerId: 6 }
-) => {
-  // console.log('createARCanvas');
+// API: createARGraphics
+const createARGraphics = (w, h, renderer = P2D, params) => {
   const cvs = createGraphics(w, h, renderer);
+  p5SimpleARCreateARCore(w, h, cvs, params);
+  return cvs;
+};
 
+// Core implementation for createARCanvas and createARGraphics
+const p5SimpleARCreateARCore = (w, h, cvs, params = { scale: 3, opacity: 1.0, markerId: 6 }) => {
   if (!params.hasOwnProperty('scale')) {
     params.scale = 3;
   }
@@ -148,15 +89,25 @@ const createARGraphics = (
     marker.appendChild(plane);
     scene.appendChild(marker);
 
-    replaceARDraw();
+    p5SimpleARReplaceARDraw();
   }
-
-  return cvs;
-
 };
 
+const p5SimpleARGetMarkerProperty = (markerId = 6) => {
+  const returnObj = {
+    markerId: markerId,
+  };
+
+  console.log(document.querySelector('#a-marker' + String(markerId)).getAttribute("rotation"))
+  console.log(document.querySelector('#a-marker' + String(markerId)).getAttribute("position"))
+
+  return returnObj;
+};
+
+// Replace draw function
 let p5SimpleARDrawReplaced = false;
-const replaceARDraw = () => {
+const p5SimpleARReplaceARDraw = () => {
+  // Avoid double replacements
   if (p5SimpleARDrawReplaced) {
     return;
   }
@@ -171,7 +122,6 @@ const replaceARDraw = () => {
     // console.log(document.querySelector('a-marker').getAttribute("position"))
 
     const planes = document.querySelectorAll('a-plane');
-    // const plane = document.querySelector('a-plane');
     planes.forEach((plane) => {
       const material = plane.getObject3D('mesh').material;
       if (material.map) {
